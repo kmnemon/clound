61dd42732de9ee4ee7b7dae787f8c2c6a88b801c
haha <haha@hahadeMacBook-Pro.local>
2020-12-14 19:23:20 +0800

    d

--- microservices/product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
@@ -10,10 +10,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.mapping.context.MappingContext;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.index.IndexOperations;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.data.mongodb.core.index.IndexResolver;
 import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
+import org.springframework.data.mongodb.core.index.ReactiveIndexOperations;
 import org.springframework.data.mongodb.core.mapping.MongoPersistentEntity;
 import org.springframework.data.mongodb.core.mapping.MongoPersistentProperty;
 import se.magnus.microservices.core.product.persistence.ProductEntity;
@@ -22,26 +22,27 @@ import se.magnus.microservices.core.product.persistence.ProductEntity;
 @ComponentScan("se.magnus")
 public class ProductServiceApplication {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ProductServiceApplication.class);
+	private static final Logger LOG = LoggerFactory.getLogger(ProductServiceApplication.class);
 
-    public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(ProductServiceApplication.class, args);
+	public static void main(String[] args) {
 
-        String mongodDbHost = ctx.getEnvironment().getProperty("spring.data.mongodb.host");
-        String mongodDbPort = ctx.getEnvironment().getProperty("spring.data.mongodb.port");
-        LOG.info("Connected to MongoDb: " + mongodDbHost + ":" + mongodDbPort);
-    }
+		ConfigurableApplicationContext ctx = SpringApplication.run(ProductServiceApplication.class, args);
 
-    @Autowired
-    MongoOperations mongoTemplate;
+		String mongodDbHost = ctx.getEnvironment().getProperty("spring.data.mongodb.host");
+		String mongodDbPort = ctx.getEnvironment().getProperty("spring.data.mongodb.port");
+		LOG.info("Connected to MongoDb: " + mongodDbHost + ":" + mongodDbPort);
+	}
 
-    @EventListener(ContextRefreshedEvent.class)
-    public void initIndicesAfterStartup() {
+	@Autowired
+	ReactiveMongoOperations mongoTemplate;
 
-        MappingContext<? extends MongoPersistentEntity<?>, MongoPersistentProperty> mappingContext = mongoTemplate.getConverter().getMappingContext();
-        IndexResolver resolver = new MongoPersistentEntityIndexResolver(mappingContext);
+	@EventListener(ContextRefreshedEvent.class)
+	public void initIndicesAfterStartup() {
 
-        IndexOperations indexOps = mongoTemplate.indexOps(ProductEntity.class);
-        resolver.resolveIndexFor(ProductEntity.class).forEach(e -> indexOps.ensureIndex(e));
-    }
+		MappingContext<? extends MongoPersistentEntity<?>, MongoPersistentProperty> mappingContext = mongoTemplate.getConverter().getMappingContext();
+		IndexResolver resolver = new MongoPersistentEntityIndexResolver(mappingContext);
+
+		ReactiveIndexOperations indexOps = mongoTemplate.indexOps(ProductEntity.class);
+		resolver.resolveIndexFor(ProductEntity.class).forEach(e -> indexOps.ensureIndex(e).block());
+	}
 }

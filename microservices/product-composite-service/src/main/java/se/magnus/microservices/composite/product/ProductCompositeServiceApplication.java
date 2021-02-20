290e08a1b3bc963b3866b8aa9e49f770ae77d4c1
ke <keliu@Kes-MacBook-Pro.local>
2021-02-20 19:42:16 +0800

    OK

--- microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -61,8 +61,6 @@ public class ProductCompositeServiceApplication {
                 ));
     }
 
-	@Autowired
-	ProductCompositeIntegration integration;
 
 	@Bean
 	@LoadBalanced
--- microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/SecurityConfig.java
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/SecurityConfig.java
@@ -1,32 +1,28 @@
 
-package spring.cloud.gateway;
+package se.magnus.microservices.composite.product;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import static org.springframework.http.HttpMethod.*;
+
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-	private static final Logger LOG = LoggerFactory.getLogger(SecurityConfig.class);
-
 	@Bean
-	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 		http
-			.csrf().disable()
 			.authorizeExchange()
-				.pathMatchers("/headerrouting/**").permitAll()
 //				.pathMatchers("/actuator/**").permitAll()
-				.pathMatchers("/eureka/**").permitAll()
-				.pathMatchers("/oauth/**").permitAll()
+				.pathMatchers(POST, "/product-composite/**").hasAuthority("SCOPE_product:write")
+				.pathMatchers(DELETE, "/product-composite/**").hasAuthority("SCOPE_product:write")
+				.pathMatchers(GET, "/product-composite/**").hasAuthority("SCOPE_product:read")
 				.anyExchange().authenticated()
 				.and()
 			.oauth2ResourceServer()
 				.jwt();
 		return http.build();
 	}
-
 }

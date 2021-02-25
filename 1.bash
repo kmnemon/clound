2abba4801951735ed275df3d3c5520bc9cfe94df
ke <keliu@Kes-MacBook-Pro.local>
2021-02-25 10:32:19 +0800

    add mvn install bash

---
+++ 1.bash
@@ -0,0 +1,7 @@
+mvn clean install -f ./microservices/product-service/pom.xml -DskipTests
+mvn clean install -f ./microservices/recommendation-service/pom.xml -DskipTests
+mvn clean install -f ./microservices/review-service/pom.xml -DskipTests
+mvn clean install -f ./microservices/product-composite-service/pom.xml -DskipTests
+mvn clean install -f ./spring-cloud/authorization-server/pom.xml -DskipTests
+mvn clean install -f ./spring-cloud/eureka-server/pom.xml -DskipTests
+mvn clean install -f ./spring-cloud/gateway/pom.xml -DskipTests
